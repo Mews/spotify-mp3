0c41a0276b278f47b22e8ad3918852ad50177918
Mews <ar754456@gmail.com>
2024-09-02 21:49:19 +0100

    Added method to fetch track urls from spotify playlist

--- src/spotifytoyt/spotify.py
+++ src/spotifytoyt/spotify.py
@@ -13,11 +13,23 @@ def create_spotify_client(client_id:str, client_secret:str) -> spotipy.Spotify:
     return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-def get_track_from_url(client:spotipy.Spotify, url:str):
+def get_track_from_url(client:spotipy.Spotify, url:str) -> Track:
     track_data = client.track(url)
 
     return track_from_spotify_data(track_data)
 
+
+def get_playlist_track_urls(client:spotipy.Spotify, playlist_url:str) -> List[str]:
+    result = client.playlist_tracks(playlist_url)
+
+    tracks = [track_data["track"]["external_urls"]["spotify"] for track_data in result["items"]]
+
+    while result["next"]:
+        result = client.next(result)
+        tracks.extend([track_data["track"]["external_urls"]["spotify"] for track_data in result["items"]])
+    
+    return tracks
+    
 
 def get_playlist_tracks(client:spotipy.Spotify, playlist_url:str) -> List[Track]:
     result = client.playlist_tracks(playlist_url)
